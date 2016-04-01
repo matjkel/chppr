787fdfd0c7edcc6ebdc297c3e3c6c73117b9a570
Patrick Lynch <patrickflynch@gmail.com>
2016-04-01 14:14:53 -0500

    add posts mock data

---
+++ testData/posts_mock_data.js
@@ -0,0 +1,62 @@
+var posts_data = [
+{
+  user_id: 1,
+  category_id: 3,
+  datetime: 1459536859876,
+  food_item_name: "Bison Burger with Jalapeno",
+  restaurant_name: "Hut's Hamburgers",
+  cost: 1325,
+  picture: '/photoStorage/burger1.png',
+  vegan: false,
+  gluten_free: false,
+  spicy: true
+},
+{
+  user_id: 2,
+  category_id: 1,
+  datetime: 1459537259695,
+  food_item_name: "Kiwi Spinach Smoothie",
+  restaurant_name: "Daily Juice",
+  cost: 749,
+  picture: '/photoStorage/kiwiSmoothie.png',
+  vegan: true,
+  gluten_free: true,
+  spicy: false  
+},
+{
+  user_id: 1,
+  category_id: 2,
+  datetime: 1459537364357,
+  food_item_name: "Rack of Lamb with Mint Jelly",
+  restaurant_name: "Roaring Fork",
+  cost: 2682,
+  picture: '/photoStorage/lamb1.png',
+  vegan: false,
+  gluten_free: false,
+  spicy: false 
+},
+{
+  user_id: 3,
+  category_id: 1,
+  datetime: 1459536859876,
+  food_item_name: "Carrot Ginger Cayenne Power Up",
+  restaurant_name: "JuiceLand",
+  cost: 801,
+  picture: '/photoStorage/juice1.png',
+  vegan: true,
+  gluten_free: true,
+  spicy: true  
+},
+{
+  user_id: 2,
+  category_id: 2,
+  datetime: 1459537552836,
+  food_item_name: "Fish and Chips",
+  restaurant_name: "B.D. Riley's",
+  cost: 1455,
+  picture: '/photoStorage/fishAndChips1.png',
+  vegan: false,
+  gluten_free: false,
+  spicy: false  
+}
+];
