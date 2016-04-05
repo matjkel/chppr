623f8d6bef79501a9503179d252474139fa25b42
Patrick Lynch <patrickflynch@gmail.com>
2016-04-05 15:59:45 -0500

    update card feed, add webpack-dev-server

--- client/components/CardFeed.js
+++ client/components/CardFeed.js
@@ -1,12 +1,17 @@
 import React from "react";
-import Card from "./Card"
+import DishCard from "./Card"
 
 export default class CardFeed extends React.Component {
   render() {
     return (
       <div>
         <h2>This will be the CardFeed Component</h2>
-        <Card/>
+        <DishCard/>
+        <DishCard/>
+        <DishCard/>
+        <DishCard/>
+        <DishCard/>
+        <DishCard/>
       </div>
     );
   }
