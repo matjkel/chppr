1b25600d484e940090499c1084489692e6b12171
Patrick Lynch <patrickflynch@gmail.com>
2016-04-10 15:03:17 -0500

    layout slightly better

--- client/components/Card.js
+++ client/components/Card.js
@@ -15,37 +15,44 @@ export default class DishCard extends React.Component {
       padding: "20px"
     };
 
+    const cardWrapperStyle = {
+      marginBottom: "30px"
+    };
+
     const buttonStyle = {
       backgroundColor: "#E9573F"
-    }    
+    };
 
     return (
-      <Card
-        className="col-md-6 col-lg-4"
-        style={cardStyle}>
-        <CardHeader
-          title={"User Number "+ this.props.data.user_id}
-          subtitle={"Category Number "+ this.props.data.category_id}
-          avatar="http://lorempixel.com/200/200/nature/"
-        />
-        <CardMedia>
-          <img src="http://lorempixel.com/600/337/food/" />
-          <p> pretend img src is: {this.props.data.picture} </p>
-        </CardMedia>
-        <CardTitle
-          title={this.props.data.food_item_name}
-          subtitle={this.props.data.restaurant_name}
-        />
-        <CardText>
-          {this.props.data.food_desc}
-        </CardText>
-        <row>
-          ${this.props.data.cost / 100}
-          {this.props.data.spicy ? " [🌶]" : ""}
-          {this.props.data.gluten_free ? " [🚫🍞]" : ""}
-          {this.props.data.vegetarian ? " [🌽]" : ""}
-        </row>
-      </Card>
+      <div className="cardWrapper col-sm-6 col-lg-4" style={cardWrapperStyle}>
+        <Card
+          className=""
+          style={cardStyle}>
+          <CardHeader
+            title={"User Number "+ this.props.data.user_id}
+            subtitle={"Category Number "+ this.props.data.category_id}
+            avatar="http://lorempixel.com/200/200/"
+          />
+          <CardMedia>
+            <img src="http://lorempixel.com/600/337/food/" />
+            <p> pretend img src is: {this.props.data.picture} </p>
+          </CardMedia>
+          <CardTitle
+            title={this.props.data.food_item_name}
+            subtitle={this.props.data.restaurant_name}
+          />
+          <row>
+            <strong>
+              ${this.props.data.cost / 100}
+            </strong>
+            <span style={{float: "right"}}>
+              {this.props.data.spicy ? " [🌶]" : ""}
+              {this.props.data.gluten_free ? " [🚫🍞]" : ""}
+              {this.props.data.vegetarian ? " [🌽]" : ""}
+            </span>
+          </row>
+        </Card>
+      </div>
     );
   }
 }
--- client/components/CardFeed.js
+++ client/components/CardFeed.js
@@ -6,16 +6,16 @@ export default class CardFeed extends React.Component {
     if (this.props.boolNoSpice && c.spicy) return false;
     if (this.props.boolGF && c.gluten_free === false) return false;
     if (this.props.boolVeg && c.vegetarian === false) return false;
-    console.log('in matchesUserFilters, card is:',c);
     return true;
   }
 
   render() {
     return (
       <div>
-        <h2>This will be the CardFeed Component</h2>
-        <p>Number of cards is: {this.props.cardData.length}</p>
-        {this.props.cardData.map((card) => this.matchesUserFilters(card) ? <DishCard data={card} /> : null)}
+        <p>Number of cards (unfiltered) is: {this.props.cardData.length}</p>
+        {this.props.cardData.map((card) => this.matchesUserFilters(card) ?
+          <DishCard data={card} /> :
+          null)}
       </div>
     );
   }
