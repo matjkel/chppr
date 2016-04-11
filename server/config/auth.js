ba9065a44545d7adc39d8e9fd12d459e8fc92227
lovelydreamer <christinamitchell08@gmail.com>
2016-04-11 10:55:31 -0500

    social share login buttons to heroku

---
+++ server/config/auth.js
@@ -0,0 +1,23 @@
+// test to confirm not in github
+
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '540646256108968', 
+        'clientSecret'  : '36b87f9e09afccb41b60c9a1f5cc33fa', 
+        'callbackURL'   : 'https://yumsnap2.herokuapp.com/dashboard'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : ' Zb8jdkKaCePMXH8yVJx2TmAZC',
+        'consumerSecret'    : 'UACmZmZQOIpLhm1OQ9lZQZTjdkrPmaXEZLEdPUddRJue96d6Mz',
+        'callbackURL'       : 'https://yumsnap2.herokuapp.com/dashboard'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'https://yumsnap2.herokuapp.com/dashboard'
+    }
+
+}
